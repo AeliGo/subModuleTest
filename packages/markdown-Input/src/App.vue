9905d36261b4ea581dfba830c6d6c95706fa7610
LI WANG <zoeywang1990@gmail.com>
2020-03-11 22:36:22 +1100

    tidy up

--- packages/markdown-Input/src/App.vue
+++ packages/markdown-Input/src/App.vue
@@ -1,32 +1,69 @@
 <template>
-  <div id="app">
-    <button @click="open = !open">open</button>
-    <markdown-markup-input v-if="open" v-model="text" :isEditing="isEditing" />
-    <markdown-markup-input v-model="text1" :isEditing="isEditing" />
+  <div class="v-editor">
+    <div class="ve-body">
+      <div class="ve-editor">
+        <pre
+          class="editor-inner markdown-highlighting"
+          @keyup="handleInputChange"
+          @focus="$emit('focus')"
+          @blur="$emit('blur')"
+        ></pre>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-import MarkdownMarkupInput from "./components/MarkdownMarkupInput";
+import "./extensions";
+import "./services/optional";
+import editorSvcFun from "./services/editorSvc";
+
 export default {
-  name: "app",
-  components: {
-    MarkdownMarkupInput
+  name: "markdown-markup-input",
+
+  props: {
+    value: {
+      type: String,
+      default: ""
+    }
   },
+
   data() {
     return {
-      open: true,
-      text: "2222",
-      text1: null,
-      isEditing: true
+      editorSvc: new editorSvcFun()
     };
   },
 
-  created() {
-    setTimeout(() => {
-      this.text1 = "# aaaaaa";
-      this.text = "# nnnnnnn";
-    });
+  methods: {
+    getContent() {
+      return this.editorSvc.clEditor.getContent();
+    },
+
+    setContent(text) {
+      this.editorSvc.clEditor.setContent(text);
+    },
+
+    handleInputChange(e) {
+      //esc,left,up,right,down key
+      if ([27, 37, 38, 39, 40].includes(e.keyCode)) return;
+      this.$emit("input", this.getContent());
+    }
+  },
+
+  watch: {
+    value() {
+      this.setContent(this.value || "");
+    }
+  },
+
+  mounted() {
+    const editorElt = this.$el.querySelector(".ve-editor .editor-inner");
+    this.editorSvc.init(editorElt);
+
+    this.setContent(this.value || "");
   }
 };
 </script>
+<style type="text/less" lang="less">
+@import "less/prism.css";
+@import "less/style";
+</style>
